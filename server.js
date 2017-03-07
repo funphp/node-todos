95856c2efa1771f066273ffe11fa0c433efbf5b7
Abdullah <ab_duetcse@yahoo.com>
2017-03-07 14:10:12 +0600

    update PUT todo

--- server.js
+++ server.js
@@ -21,17 +21,19 @@ app.get('/todos', function(req, res) {
     if (query.hasOwnProperty('completed') && query.completed == 'true') {
         where.completed = true;
     } else if (query.hasOwnProperty('completed') && query.completed == 'false') {
-       where.completed = false;
+        where.completed = false;
     }
     if (query.hasOwnProperty('q') && query.q.trim().length > 0) {
         where.description = {
-            $like : '%'+query.q+'%'
+            $like: '%' + query.q + '%'
         }
     }
 
-    db.todo.findAll({where:where}).then(function(todos){
+    db.todo.findAll({
+        where: where
+    }).then(function(todos) {
         res.json(todos);
-    },function(error){
+    }, function(error) {
         res.status(500).send();
     });
 
@@ -41,7 +43,7 @@ app.get('/todos', function(req, res) {
 app.get('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
 
-    db.todo.findById(todoId).then(function(todo){
+    db.todo.findById(todoId).then(function(todo) {
         if (!!todo) {
             res.json(todo.toJSON());
         } else {
@@ -83,30 +85,20 @@ app.post('/todos', function(req, res) {
 //delete todo
 app.delete('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    /*var todo = _.findWhere(todos, {
-        id: todoId
-    });
-
-    if (todo === '') {
-        res.status(404).send();
-    } else {
-        todos = _.without(todos, todo);
-        res.json(todo);
-    }*/
     //
     db.todo.destroy({
         where: {
-            id:todoId
+            id: todoId
         }
-    }).then(function(rowsDeleted){
-        if(rowsDeleted == 0) {
+    }).then(function(rowsDeleted) {
+        if (rowsDeleted == 0) {
             res.status(404).json({
                 error: 'no todo with this id'
             });
         } else {
             res.status(200).send();
         }
-    }, function(error){
+    }, function(error) {
         res.status(500).send();
     });
 
@@ -117,7 +109,7 @@ app.delete('/todos/:id', function(req, res) {
 app.put('/todos/:id', function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
     var todoId = parseInt(req.params.id, 10);
-    var todo = _.findWhere(todos, {
+    /*var todo = _.findWhere(todos, {
         id: todoId
     });
 
@@ -140,7 +132,39 @@ app.put('/todos/:id', function(req, res) {
     }
 
     _.extend(todo, validAttributes);
-    res.json(todo);
+    res.json(todo);*/
+    //update todo in porstgres
+
+    var validAttributes = {};
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAttributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        console.log('completed');
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAttributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        console.log('description');
+        return res.status(400).send();
+    }
+
+    db.todo.findById(todoId).then(function(todo) {
+            if (!!todo) {
+                return todo.update(validAttributes);
+            } else {
+                res.status(404).send();
+            }
+        }, function(error) {
+            res.status(500).send();
+        })
+        .then(function(todo) {
+            res.json(todo.toJSON());
+        }, function(error) {
+            res.status(400).json(e);
+        });
+
 
 });
 
